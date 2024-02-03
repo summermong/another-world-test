4487cfe840df525f1f7a40ff53d4065571a5a58d
summermong <2514o_o@naver.com>
2024-02-03 16:18:31 +0900

    :lipstick: STYLE: 전체 결과 카드 컴포넌트 이미지 배치 수정

--- src/stories/ResultCard.jsx
+++ src/stories/ResultCard.jsx
@@ -2,21 +2,34 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 const ResultCard = styled.div`
-  width: 300px;
-  background-color: white;
-  box-shadow: 0 0 7px #df7abe;
+  width: 310px;
   display: flex;
-  margin: 5px;
-  justify-content: space-around;
+  margin: 5px auto;
   border-radius: 5px;
   cursor: pointer;
+  color: white;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const ImgWrapper = styled.section`
+  width: 90px;
+  height: 90px;
+  display: flex;
+  margin: 15px auto;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #fff;
+  border-radius: 50%;
+  box-shadow:
+    0px 0px 15px #bf8df2,
+    0px 0px 15px #df7abe;
 `;
 
 const ResultImg = styled.img`
-  width: 90px;
-  height: 90px;
-  padding: 10px;
-  margin: 5px;
+  width: 60px;
+  height: 60px;
 `;
 
 const ResultInfo = styled.div`
@@ -25,7 +38,6 @@ const ResultInfo = styled.div`
   font-size: 12px;
   display: flex;
   flex-direction: column;
-  gap: 10px;
   text-align: center;
   justify-content: center;
 `;
@@ -33,12 +45,15 @@ const ResultInfo = styled.div`
 const ResultTitle = styled.div`
   font-size: 16px;
   font-weight: 700;
+  text-shadow: 0px 0px 3px #df7abe;
 `;
 
 const PersonalityCard = ({ src, title, sub, onClick }) => {
   return (
     <ResultCard onClick={onClick}>
-      <ResultImg src={src} />
+      <ImgWrapper>
+        <ResultImg src={src} />
+      </ImgWrapper>
       <ResultInfo>
         <ResultTitle>{title}</ResultTitle>
         <div>{sub}</div>
