afe61f8014997b8caebe06d55caf857d7470e031
summermong <2514o_o@naver.com>
2024-02-03 16:17:44 +0900

    :lipstick: STYLE: 전체 배경 어둡게 수정

--- src/style/GlobalStyles.js
+++ src/style/GlobalStyles.js
@@ -5,6 +5,27 @@ const GlobalStyles = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-}`;
+
+}
+
+body {
+  width: 100vw;
+  height: 100vh;
+  background-image: url('/img/background/background.jpeg');
+  background-size: cover;
+  background-position: center center;
+  position: relative;
+}
+
+body::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.55); 
+}
+`;
 
 export default GlobalStyles;
