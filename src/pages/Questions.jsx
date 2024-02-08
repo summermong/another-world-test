90d5abebe3c40cf12f9b37241f1a288420e67359
summermong <2514o_o@naver.com>
2024-02-08 22:42:07 +0900

    :sparkles: FEAT: 질문 넘어가는 모션

--- src/pages/Questions.jsx
+++ src/pages/Questions.jsx
@@ -4,6 +4,7 @@ import { ChoiceButton } from '../stories/Button.stories';
 import questionData from '../data/questionData';
 import { useNavigate } from 'react-router-dom';
 import BarLoader from 'react-spinners/BarLoader';
+import { motion, AnimatePresence } from 'framer-motion';
 
 const Questions = () => {
   const navigate = useNavigate();
@@ -23,7 +24,31 @@ const Questions = () => {
     borderRadius: '10px',
   };
 
+  const boxVariants = {
+    entry: back => ({
+      x: back ? 300 : -300,
+      opacity: 0,
+      transition: { duration: 0.5 },
+      scale: 0.8,
+    }),
+    center: {
+      opacity: 1,
+      x: 0,
+      transition: { duration: 0.5 },
+      scale: 1,
+    },
+    exit: back => ({
+      x: back ? -300 : 300,
+      opacity: 0,
+      scale: 0.8,
+      transition: { duration: 0.5 },
+    }),
+  };
+
+  const [back, setBack] = useState(false);
+
   const handleNextQ = type => {
+    setBack(false);
     if (curQIdx + 1 < questionData.length) {
       setCurQIdx(prevIdx => prevIdx + 1);
 
@@ -62,6 +87,7 @@ const Questions = () => {
   };
 
   const handlePrevQ = () => {
+    setBack(true);
     if (curQIdx > 0) {
       setCurQIdx(prevIdx => {
         setWidth(((prevIdx - 1) / questionData.length) * 250);
@@ -72,43 +98,56 @@ const Questions = () => {
 
   return (
     <QuestionContainer>
-      {isLoadingOpen ? (
-        <LoadingSpinner>
-          이세계로 로딩중...
-          <BarLoader
-            color="#bf8df2"
-            height="10px"
-            width="250px"
-            loading={setIsLoadingOpen}
-            aria-label="Loading Spinner"
-            cssOverride={css}
-            speedMultiplier="1"
-          />
-        </LoadingSpinner>
-      ) : (
-        <>
-          <QuestionBox>
-            <QuestionNum>Q. {curQ.Num}</QuestionNum>
-            <div>{curQ.Q}</div>
-          </QuestionBox>
-          <BarAndPrevWrapper>
-            <PreButton onClick={handlePrevQ}>◀️</PreButton>
-            <ProgressBar>
-              <Progress width={width} />
-            </ProgressBar>
-          </BarAndPrevWrapper>
-          <AnswerBox>
-            {curQ.A.map((answer, idx) => (
-              <ChoiceButton
-                key={idx}
-                onClick={() => handleNextQ(answer.type)}
-                label={answer.text}
-                answer={answer}
-              />
-            ))}
-          </AnswerBox>
-        </>
-      )}
+      <AnimatePresence mode="wait" custom={back}>
+        {isLoadingOpen ? (
+          <LoadingSpinner>
+            이세계로 로딩중...
+            <BarLoader
+              color="#bf8df2"
+              height="10px"
+              width="250px"
+              loading={setIsLoadingOpen}
+              aria-label="Loading Spinner"
+              cssOverride={css}
+              speedMultiplier="1"
+            />
+          </LoadingSpinner>
+        ) : (
+          <motion.div
+            custom={back}
+            variants={boxVariants}
+            initial="entry"
+            animate="center"
+            exit="exit"
+            key={curQIdx}
+          >
+            <QuestionBox>
+              <QuestionNum>Q. {curQ.Num}</QuestionNum>
+              <div>{curQ.Q}</div>
+            </QuestionBox>
+
+            <BarAndPrevWrapper>
+              <PreButton onClick={handlePrevQ}>◀️</PreButton>
+              <ProgressBar>
+                <Progress width={width}>
+                  {width === 0 ? '' : ((width / 250) * 100).toFixed(0) + '%'}
+                </Progress>
+              </ProgressBar>
+            </BarAndPrevWrapper>
+
+            <AnswerBox>
+              {curQ.A.map((answer, idx) => (
+                <ChoiceButton
+                  key={idx}
+                  onClick={() => handleNextQ(answer.type)}
+                  label={answer.text}
+                  answer={answer}
+                />
+              ))}
+            </AnswerBox>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </QuestionContainer>
   );
 };
@@ -160,11 +199,12 @@ const BarAndPrevWrapper = styled.div`
 
 const ProgressBar = styled.div`
   width: 250px;
-  height: 10px;
+  height: 20px;
   background-color: #fff;
   border-radius: 50px;
   box-shadow: 0 0 10px #df7abe;
   margin: 20px 0px;
+  font-size: 12px;
 `;
 
 const PreButton = styled.button`
@@ -182,9 +222,13 @@ const PreButton = styled.button`
 
 const Progress = styled.div`
   width: ${props => props.width}px;
-  height: 10px;
+  height: 20px;
   background-color: #bf8df2;
   border-radius: 50px;
+  display: flex;
+  text-align: center;
+  align-items: center;
+  justify-content: center;
 `;
 
 const AnswerBox = styled.div`
