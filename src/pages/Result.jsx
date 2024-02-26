8fc6edb34839c0e65309815a2662ec02146c6221
summermong <2514o_o@naver.com>
2024-02-26 17:43:20 +0900

    :bug: FIX: 페이지별 공유하기 썸네일 크기 수정중

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -74,7 +74,7 @@ const Result = () => {
         content: {
           title: `${resultData[testResult]?.title}`,
           description: '이세계에서의 직업을 확인해보세요!',
-          imageUrl: `${import.meta.env.VITE_WEBSITE_URL}${resultData[testResult]?.src}`,
+          imageUrl: `${import.meta.env.VITE_WEBSITE_URL}/img/thumbnail/${testResult}.png`,
           imageWidth: 300,
           imageHeight: 300,
           link: {
