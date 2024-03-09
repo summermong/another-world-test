8034a49197a322cf4bd08b6635c2ef87ad6182f0
summermong <2514o_o@naver.com>
2024-03-09 22:12:39 +0900

    :bug: FIX: render delay debug (음악 파일 압축)

--- src/App.tsx
+++ src/App.tsx
@@ -57,17 +57,17 @@ function App() {
     };
   }, [music]);
 
-  // useEffect(() => {
-  //   const script = document.createElement('script');
-  //   script.src = 'https://developers.kakao.com/sdk/js/kakao.js';
-  //   script.async = true;
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://developers.kakao.com/sdk/js/kakao.js';
+    script.async = true;
 
-  //   document.body.appendChild(script);
+    document.body.appendChild(script);
 
-  //   return () => {
-  //     document.body.removeChild(script);
-  //   };
-  // }, []);
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
 
   return (
     <AppContainer>
