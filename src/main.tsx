a1ffd24ddf2002935207ee924677c4f3d1687e46
summermong <2514o_o@naver.com>
2024-03-16 23:58:23 +0900

    :bug: FIX: main.tsx에서도 react-helmet-async 제거

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { HelmetProvider } from 'react-helmet-async';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <HelmetProvider>
-    <App />
-  </HelmetProvider>,
-);
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />);
